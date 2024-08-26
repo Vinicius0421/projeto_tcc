4663cb859b38136e68ff4af3c31877d0f1fde5bc
Kaua61 <154093087+Kaua61@users.noreply.github.com>
2024-08-26 14:39:46 -0300

    Update db_yummifoods.sql

--- db_yummifoods.sql
+++ db_yummifoods.sql
@@ -5,7 +5,7 @@ CREATE TABLE cliente (
 CPF char(11) NOT NULL,
 nome_cliente varchar(60) NOT NULL,
 primary key(CPF)
-);
+)engine=InnoDB default charset=utf8mb4;
 
 CREATE TABLE pedido (
     IDpedido INT AUTO_INCREMENT PRIMARY KEY,
@@ -14,19 +14,18 @@ CREATE TABLE pedido (
     id_cliente CHAR(11) NOT NULL,
     preco_total DECIMAL(10, 2) NOT NULL,
    foreign key (id_cliente) references cliente(CPF)
-);
+)engine=InnoDB default charset=utf8mb4;
  -- (gustavo guanabara): alter table pedidos add foreign key (id_cliente) references clientes(CPF);
  
  create table produtos (
  IDproduto int auto_increment not null PRIMARY KEY,
- nome_produto varchar(50) not null,
+ nome_produto varchar(255) not null,
  preco_unitario decimal (10, 2) not null,
  quantidade int not null,
- status boolean,
- imagem blob
- );
-
- -- revisar os valores do varchar
+ status_pedido boolean not null,
+ imagem blob 
+ )engine=InnoDB default charset=utf8mb4;
+ 
  
  create table detalhes_pedido (
  id_pedido int  not null,
@@ -34,27 +33,23 @@ CREATE TABLE pedido (
  quantidade int not null,
  foreign key (id_pedido) references pedido(IDpedido),
  foreign key (id_produto) references produtos(IDproduto)
- );
- -- falta colocar as fk ^
+ )engine=InnoDB default charset=utf8mb4;
+ -- revisar se a quantidade é chave estrangeira (tabela produtos)
  
  create table funcionarios(
  CPF_funcionario char(11) NOT NULL PRIMARY KEY,
- nome_funcionario varchar(255) not null,
+ nome_funcionario varchar(80) not null,
  email_funcionario varchar(255) not null,
  senha_funcionario varchar(255) not null
- );
- 
- describe cliente;
- describe detalhes_pedido;
- describe funcionarios;
- describe pedido;
- describe produtos;
+ )engine=InnoDB default charset=utf8mb4;
  
 
 
  
 
  
+ 
+
 
 
 
